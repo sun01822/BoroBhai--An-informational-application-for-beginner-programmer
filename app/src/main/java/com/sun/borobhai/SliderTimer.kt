01e10cf8dc58a7cbd9034f708f6070aae760401c
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-07-20 00:23:29 +0600

    Image slider and logout action added successfully

---
+++ app/src/main/java/com/sun/borobhai/SliderTimer.kt
@@ -0,0 +1,17 @@
+package com.sun.borobhai
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+
+class SliderTimer(private val recyclerView: RecyclerView) : TimerTask() {
+
+    override fun run() {
+        recyclerView.post {
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val currentPosition = layoutManager.findFirstVisibleItemPosition()
+            val newPosition = if (currentPosition == recyclerView.adapter?.itemCount?.minus(1)) 0 else currentPosition + 1
+            recyclerView.smoothScrollToPosition(newPosition)
+        }
+    }
+}
