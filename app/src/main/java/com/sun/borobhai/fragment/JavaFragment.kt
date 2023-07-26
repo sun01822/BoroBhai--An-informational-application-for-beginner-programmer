0794210520836d4a55590d7807d1e49bcc11a642
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-07-26 09:05:00 +0600

    Shorten the code

--- app/src/main/java/com/sun/borobhai/fragment/JavaFragment.kt
+++ app/src/main/java/com/sun/borobhai/fragment/JavaFragment.kt
@@ -5,12 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sun.borobhai.R
 import com.sun.borobhai.databinding.FragmentJavaBinding
-import com.sun.borobhai.helper.FragmentHelper
-import com.sun.borobhai.helper.FragmentHelper.loadJSONFromAsset
-import com.sun.borobhai.helper.FragmentHelper.parseLanguageDataFromJSON
-import com.sun.borobhai.helper.FragmentHelper.setupRecyclerView
+import com.sun.borobhai.helper.FragmentHelper.fetchDataFromJsonFile
 
 class JavaFragment : Fragment() {
     private lateinit var binding : FragmentJavaBinding
@@ -22,47 +18,11 @@ class JavaFragment : Fragment() {
 
         val value = arguments?.getString("value_key")
 
-        val jsonString = loadJSONFromAsset(requireContext(), "data.json")
-        val languageData = parseLanguageDataFromJSON(jsonString?.toString(), value!!)
-
-        languageData?.let {
-            binding.tvLanguageName.text = it.name
-            binding.tvLanguageDefinition.text = it.definition
-            binding.tvWhyLearn.text = it.whyLearn
-
-            setupRecyclerView(
-                requireContext(),
-                binding.rvBestBooks,
-                it.bestBooks,
-                it.booksDownloadLinks,
-                0,
-                0
-            )
-            setupRecyclerView(
-                requireContext(),
-                binding.rvBestEditors,
-                it.bestEditors,
-                it.editorsDownloadLinks,
-                1,
-                0
-            )
-            setupRecyclerView(
-                requireContext(),
-                binding.rvBestYouTubeChannels,
-                it.bestYouTubeChannels,
-                it.youtubeChannelsLinks,
-                2,
-                1
-            )
-            setupRecyclerView(
-                requireContext(),
-                binding.rvOnlineCompilers,
-                it.onlineCompilers,
-                it.onlineCompilersLink,
-                3,
-                0
-            )
-        }
+        fetchDataFromJsonFile(
+            requireContext(), value.toString(), binding.tvLanguageName,
+            binding.tvLanguageDefinition, binding.tvWhyLearn, binding.rvBestBooks,
+            binding.rvBestEditors, binding.rvBestYouTubeChannels, binding.rvOnlineCompilers
+        )
         return binding.root
     }
 }
