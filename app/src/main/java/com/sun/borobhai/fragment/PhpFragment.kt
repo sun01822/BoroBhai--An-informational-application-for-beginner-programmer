49bf88aecd0f5a1023ddcd6f18e39aa12fdc82a4
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-08-04 10:04:08 +0600

    Some XML file is deleted

--- app/src/main/java/com/sun/borobhai/fragment/PhpFragment.kt
+++ app/src/main/java/com/sun/borobhai/fragment/PhpFragment.kt
@@ -5,23 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sun.borobhai.databinding.FragmentPhpBinding
+import com.sun.borobhai.databinding.FragmentLanguagesBinding
 import com.sun.borobhai.helper.FragmentHelper.fetchDataFromJsonFile
 
 class PhpFragment : Fragment() {
-    private lateinit var binding : FragmentPhpBinding
+    private lateinit var binding: FragmentLanguagesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentPhpBinding.inflate(layoutInflater)
-
+        binding = FragmentLanguagesBinding.inflate(layoutInflater)
         val value = arguments?.getString("value_key")
-        fetchDataFromJsonFile(
-            requireContext(), value.toString(), binding.tvLanguageName,
+
+        fetchDataFromJsonFile(requireContext(), value.toString(), binding.tvLanguageName,
             binding.tvLanguageDefinition, binding.tvWhyLearn, binding.rvBestBooks,
-            binding.rvBestEditors, binding.rvBestYouTubeChannels, binding.rvOnlineCompilers
-        )
+            binding.rvBestEditors, binding.rvBestYouTubeChannels, binding.rvOnlineCompilers)
+
         return binding.root
     }
 }
